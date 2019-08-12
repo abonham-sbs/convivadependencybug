d6c5f5b92ab9e5173c04586ed3ac5f81401f6b29
Aaron Bonham <aaron.bonham@sbs.com.au>
2019-08-13 09:23:13 +1000

    initial commit

---
+++ Example/Pods/Target Support Files/ConvivaDependency/ConvivaDependency-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ConvivaDependency : NSObject
+@end
+@implementation PodsDummy_ConvivaDependency
+@end
---
+++ Example/Pods/Target Support Files/Pods-ConvivaDependency_Example/Pods-ConvivaDependency_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ConvivaDependency_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ConvivaDependency_Example
+@end
